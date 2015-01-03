a29c3724615cce825af274b943c9e68cc5f00a19
Nick Winter <livelily@gmail.com>
2015-01-03 11:10:02 -0800

    Updated to latest Flask App Engine template.

---
+++ src/application/models.py
@@ -0,0 +1,17 @@
+"""
+models.py
+
+App Engine datastore models
+
+"""
+
+
+from google.appengine.ext import ndb
+
+
+class ExampleModel(ndb.Model):
+    """Example Model"""
+    example_name = ndb.StringProperty(required=True)
+    example_description = ndb.TextProperty(required=True)
+    added_by = ndb.UserProperty()
+    timestamp = ndb.DateTimeProperty(auto_now_add=True)
